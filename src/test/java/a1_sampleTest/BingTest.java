df6d47d1231b1112097e0e78e4213d7902e958ef
umadevitamilselvan2330 <umadevitamilselvan2330@gmail.com>
2022-09-15 09:35:07 +0530

    first commit

--- src/test/java/a1_sampleTest/BingTest.java
+++ src/test/java/a1_sampleTest/BingTest.java
@@ -1,5 +1,33 @@
 package a1_sampleTest;
 
-public class BingTest {
 
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BingTest {
+	
+	@Test
+	public void validateBing() throws IOException{
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd = new ChromeDriver();
+		wd.get("https://www.bing.com/");
+		wd.findElement(By.name("q")).sendKeys("chennai");
+		File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src, new File("D:/workspace/J1_GitTest/screenshot"));
+		
+	}
+  // one suite matum na that partuicullar file la right click panni TestNg and convert TestNg kuduthutu panalam
+ // but all suite pananum na antha code a google la search (multiole suite using TestNg) search panni antha code a copy pannitu 
+//then theriumbavum oru file right click pannitu convert TestNg pannitu but suite file la 2 suite irukum antha 2nd suite kum name vachu run panni pakanum
 }
--- src/test/java/a2_sampleTest/GoogleTest.java
+++ src/test/java/a2_sampleTest/GoogleTest.java
@@ -1,5 +1,30 @@
 package a2_sampleTest;
 
-public class GoogleTest {
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class GoogleTest{
+	
+	public void googleTest() throws IOException{
+		
+	WebDriverManager.chromedriver().setup();
+	WebDriver wd = new ChromeDriver();
+	wd.get("https://www.bing.com/");
+	wd.findElement(By.name("q")).sendKeys("chennai");
+	File src=((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+	FileHandler.copy(src, new File("D:/workspace/J1_GitTest/screenshot"));
+	
+}
 
 }
